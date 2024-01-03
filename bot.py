bdad35b11be3831d67df5194f8702359eb14bef2
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-01-03 10:29:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from aiogram import F, Bot, Dispatcher, Router, types
 
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
 from aiogram.filters.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
@@ -26,7 +26,6 @@ from DataManager import logger_config
 
 class MyBot:
     def __init__(self, base_dir, bot):
-        self.bot_token = config.bot_token
         self.admin_id = config.admin_id
         self.base_dir = base_dir
         self.save_dir = r""
@@ -57,15 +56,17 @@ class MyBot:
         self.dp.callback_query(F.data.startswith('enter_folder:'))(self.handle_enter_folder_callback)
         self.dp.callback_query(F.data.startswith('move_to_folder:'))(self.handle_move_to_folder_callback)
         self.dp.message(F.text)(self.filter_entries)
+        self.dp.message(Command("create_folder"))(self.handle_create_folder)
+        self.dp.message(CreateFolderState.name)(self.handle_folder_name)
         
     async def command_start_handler(self, message: Message) -> None:
         user_id = UserInfo(message).user_id
         try:
             if user_id == self.admin_id:
                 keyboard = self.folder_manager.list_entries(None, 0)
-                await message.answer("Seleziona un elemento:", reply_markup=keyboard)
+                await message.answer("Select element:", reply_markup=keyboard)
         except Exception as ex:
-            logging.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logging.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{user_id}:{ex}")  
 
     async def handle_message(self, message: types.Message):
@@ -73,10 +74,10 @@ class MyBot:
         try:
             if user_id == self.admin_id:
                     file_path, file_name = await self.folder_manager.ReciveImage(message)
-                    await message.answer(f"File salvato in {file_path}")
+                    await message.answer(f"File saved in {file_path}")
                     self.folder_manager.save_file(file_path, self.save_dir)
         except Exception as ex:
-            logging.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logging.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
         return      
             
@@ -84,9 +85,9 @@ class MyBot:
         try:
             page = int(callback_query.data.split(':')[1])
             keyboard = self.folder_manager.list_entries(None, page)
-            await self.bot.edit_message_text("Seleziona un elemento:", callback_query.from_user.id, callback_query.message.message_id, reply_markup=keyboard)
+            await self.bot.edit_message_text("Select element:", callback_query.from_user.id, callback_query.message.message_id, reply_markup=keyboard)
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
             
     async def handle_folder_callback(self, callback_query: types.CallbackQuery):
@@ -95,26 +96,26 @@ class MyBot:
             folder_name = self.db.get_entry(folder_id)
             self.folder_manager.change_folder(folder_name)
             keyboard = self.folder_manager.list_entries(None, 0)
-            await callback_query.message.edit_text("Seleziona un elemento:", reply_markup=keyboard)
+            await callback_query.message.edit_text("Select element:", reply_markup=keyboard)
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
             
     async def handle_back_callback(self, callback_query: types.CallbackQuery):
         try:    
             self.folder_manager.go_back()
             keyboard = self.folder_manager.list_entries(None, 0)
-            await callback_query.message.edit_text("Seleziona un elemento:", reply_markup=keyboard)
+            await callback_query.message.edit_text("Select element:", reply_markup=keyboard)
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
             
     async def handle_cancel_callback(self, callback_query: types.CallbackQuery):
         try:
             user_id = callback_query.from_user.id
-            await callback_query.message.edit_text("Operazione annullata")
+            await callback_query.message.edit_text("Operation delated")
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
         
     async def handle_action_file_callback(self, callback_query: types.CallbackQuery, state: FSMContext):
@@ -125,9 +126,9 @@ class MyBot:
         
             await state.update_data(file_name=file_name) # Puoi anche salvare dei dati nello stato
             keyboard = self.folder_manager.file_actions(file_id)
-            await callback_query.message.edit_text(f"Azioni per il file {file_name}:", reply_markup=keyboard)
+            await callback_query.message.edit_text(f"Action for file {file_name}:", reply_markup=keyboard)
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
         
     async def handle_action_folder_callback(self, callback_query: types.CallbackQuery, state: FSMContext):
@@ -137,9 +138,9 @@ class MyBot:
             await state.set_state(MoveFileState.file_name)
             await state.update_data(folder_name=folder_name)
             keyboard = self.folder_manager.folder_actions(folder_id)
-            await callback_query.message.edit_text(f"Azioni per la cartella {folder_name}:", reply_markup=keyboard)
+            await callback_query.message.edit_text(f"Action for folder {folder_name}:", reply_markup=keyboard)
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")         
         
     async def handle_enter_folder_callback(self, callback_query: types.CallbackQuery):
@@ -149,9 +150,9 @@ class MyBot:
             if self.folder_manager.change_folder(folder_name):
                 await self.move.show_destination_folders(callback_query)
             else:
-                await callback_query.message.edit_text("Impossibile accedere alla cartella selezionata")
+                await callback_query.message.edit_text("cannot open directory")
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
         
     async def handle_move_to_folder_callback(self, callback_query: types.CallbackQuery, state: FSMContext):
@@ -160,7 +161,7 @@ class MyBot:
             folder_path = self.folder_manager.get_entry(folder_id)
             await self.move.handle_move_to_folder_callback(callback_query, folder_path, state)
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}") 
             
     async def filter_entries(self, message: types.Message):
@@ -169,8 +170,23 @@ class MyBot:
             user_id = message.from_user.id
             if user_id == self.admin_id:
                 keyboard = self.folder_manager.list_entries(filter_text, 0)
-                await message.answer("Seleziona un elemento:", reply_markup=keyboard)
+                await message.answer("Select element:", reply_markup=keyboard)
         except Exception as ex:
-            logger_config.logger.error(f"Errore durante l'esecuzione di command_start_handler: {ex}", exc_info=True)
+            logger_config.logger.error(f"{ex}", exc_info=True)
             await self.bot.send_message(self.admin_id, f"{ex}")  
+
+    async def handle_create_folder(message: types.Message, state: FSMContext):
+        await CreateFolderState.name.set()
+        await message.answer("Please enter the name of the folder you want to create:")
+
+    async def handle_folder_name(message: types.Message, state: FSMContext):
+        save_dir = ""
+        folder_name = os.path.join(save_dir, message.text)
+        try:
+            os.mkdir(folder_name)
+            await message.answer(f"Folder {folder_name} created succesfully!")
+        except Exception as ex:
+            await message.answer(f"{ex}")
+        finally:        
+            await state.clear()        
     
