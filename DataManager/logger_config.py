b8a74f37244cce127a82d45d4bf739d471860a0c
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-01-03 10:37:19 +0100

    Update logger_config.py

--- DataManager/logger_config.py
+++ DataManager/logger_config.py
@@ -2,22 +2,21 @@ import logging
 import sys
 from DataManager import config
 
-# Configurazione del logger
+# Logger configuration
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=config.log_level,  # Imposta su DEBUG in modo da includere tutti i livelli di log
 )
 
-# Crea un logger
 logger = logging.getLogger(__name__)
 
-# Imposta un gestore per il livello INFO
+# Set a handler for the INFO level
 info_handler = logging.StreamHandler(sys.stdout)
 info_handler.setLevel(logging.INFO)
 info_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
 logger.addHandler(info_handler)
 
-# Imposta un gestore per il livello ERROR
+# Set a handler for the ERROR level
 error_handler = logging.FileHandler(config.log_file_path, mode='a')
 error_handler.setLevel(logging.ERROR)
 error_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
