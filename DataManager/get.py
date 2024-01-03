c95298ecde884cec892a3a76a871433c0c2fb2e5
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-01-03 10:35:39 +0100

    Update get.py

--- DataManager/get.py
+++ DataManager/get.py
@@ -9,24 +9,17 @@ class Get:
         self.folder_manager = folder_manager
         self.db = Database()
 
-    # async def process_callback_get(self, callback_query: types.CallbackQuery):
-    #     file_id = int(callback_query.data.split(':')[1])
-    #     file_name = self.folder_manager.get_entry(file_id)
-    #     file_path = os.path.join(self.folder_manager.current_path, file_name)
-    #     input_file = types.FSInputFile(file_path)  # Crea un oggetto FSInputFile dal percorso del file
-    #     await self.bot.send_document(callback_query.from_user.id, input_file)
-
     async def process_callback_get(self, callback_query: types.CallbackQuery):
         file_id = int(callback_query.data.split(':')[1])
         file_name = self.db.get_entry(file_id)
         file_path = os.path.join(self.folder_manager.current_path, file_name)
         if os.path.isdir(file_path):
             zip_path = shutil.make_archive(file_path, 'zip', file_path)
-            input_file = types.FSInputFile(file_path)  # Crea un oggetto FSInputFile dal percorso del file
+            input_file = types.FSInputFile(file_path)
             await self.bot.send_document(callback_query.from_user.id, input_file)
             os.remove(zip_path)
         else:
-            input_file = types.FSInputFile(file_path)  # Crea un oggetto FSInputFile dal percorso del file
+            input_file = types.FSInputFile(file_path)
             await self.bot.send_document(callback_query.from_user.id, input_file)
 
 
