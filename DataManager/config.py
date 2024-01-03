2f4e4e777540324d9229b3fedf36692056b02c8c
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-01-03 03:53:10 +0100

    Update config.py

--- DataManager/config.py
+++ DataManager/config.py
@@ -1,18 +1,18 @@
 import logging
 
-bot_token = '6260189061:AAGfrCYpQMhBrwVV5riAyH8ESYV5Q0fz4Kw' #token of test
+bot_token = 'YOUR-TOKEN' #token of test
 
-#bot_token = '6067993693:AAFXGtO7iXsjxJK0dBv8YuT-zgNcpQTU06g'
+#bot_token = 'YOUR-TOKEN' #token of production
 
-admin_id = 1026795763
+admin_id = 'YOUR-CHAT_ID'
 
 api_base_url = 'http://localhost:8081'  
 
 log_file_path = 'log.txt'  
 
-use_local_api = False  # Imposta su False se l'API non è in locale
+use_local_api = False   # Set to False if the API is not in local
 
-log_level = logging.INFO  # Livello di logging desiderato
+log_level = logging.INFO  # Desired logging level, INFO, ERROR OR DEBUG
 
 
 class FileExtensions:
@@ -26,7 +26,8 @@ class FileExtensions:
             '.zip' : 'Zip',
             '.html': 'Note',
             '.txt': 'Note'
-            # Aggiungi qui altri tipi di file se necessario
+            # Add other file types here if needed
+            # here is where the files are saved in the folders listed
         }
 
     def get_folder(self, extension):
